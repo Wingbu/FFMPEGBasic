c2b17086b8040fd307def770a34fe9d46351f1ea
Wingbu <949540717@qq.com>
2018-08-20 23:41:58 +0800

    add view play in mainActivity

--- app/src/main/cpp/native-player.cpp
+++ app/src/main/cpp/native-player.cpp
@@ -8,123 +8,167 @@ extern "C" {
     #include "libavcodec/avcodec.h"
     #include "libavformat/avformat.h"
     #include "libswscale/swscale.h"
+    #include "libavutil/imgutils.h"
 }
 
 #define FFLOGI(FORMAT,...) __android_log_print(ANDROID_LOG_INFO,"ffmpeg",FORMAT,##__VA_ARGS__);
 #define FFLOGE(FORMAT,...) __android_log_print(ANDROID_LOG_ERROR,"ffmpeg",FORMAT,##__VA_ARGS__);
+#define LOG_TAG    "ffmpegandroidplayer"
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 
 extern "C"
 JNIEXPORT jint JNICALL
 Java_com_example_wingbu_ffmpegbasic_play_VideoPlayActivity_play(JNIEnv *env, jclass type, jstring input_,
                                                      jobject surface) {
 //    const char *videoFile = env->GetStringUTFChars(input_, 0);
-//    FFLOGI("%s",videoFile);
-
-//    av_register_all();
 //
+//    LOGD("play");
+//
+//    // sd卡中的视频文件地址,可自行修改或者通过jni传入
+//    //char *file_name = "/storage/emulated/0/ws2.mp4";
+//    const char *file_name = env->GetStringUTFChars(input_, 0);
+//
+//    av_register_all();
 //
 //    AVFormatContext *pFormatCtx = avformat_alloc_context();
 //
-//    if(avformat_open_input(&pFormatCtx,videoFile,NULL,NULL) != 0){
-//       FFLOGI("Couldn't open file:%s\n",videoFile);
-//       return -1;
+//    // Open video file
+//    if (avformat_open_input(&pFormatCtx, file_name, NULL, NULL) != 0) {
+//
+//        LOGD("Couldn't open file:%s\n", file_name);
+//        return -1; // Couldn't open file
 //    }
 //
-//    if(avformat_find_stream_info(pFormatCtx,NULL) > 0){
-//       FFLOGI("Couldn't find stream information.");
-//       return -1;
+//    // Retrieve stream information
+//    if (avformat_find_stream_info(pFormatCtx, NULL) < 0) {
+//        LOGD("Couldn't find stream information.");
+//        return -1;
 //    }
 //
-//    int videoStream = -1,i;
-//    for(i = 0 ; i < pFormatCtx -> nb_streams; i++){
-//       if( pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO && videoStream < 0){
-//           videoStream = i;
-//       }
+//    // Find the first video stream
+//    int videoStream = -1, i;
+//    for (i = 0; i < pFormatCtx->nb_streams; i++) {
+//        if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO
+//            && videoStream < 0) {
+//            videoStream = i;
+//        }
 //    }
-//
 //    if (videoStream == -1) {
-//         FFLOGI("Didn't find a video stream.");
-//         return -1; // Didn't find a video stream
+//        LOGD("Didn't find a video stream.");
+//        return -1; // Didn't find a video stream
 //    }
 //
-//    AVCodecContext *pCodecContext = pFormatCtx->streams[videoStream]->codec;
+//    // Get a pointer to the codec context for the video stream
+//    AVCodecContext *pCodecCtx = pFormatCtx->streams[videoStream]->codec;
 //
-//    AVCodec *pCodec = avcodec_find_decoder(pCodecContext->codec_id);
-//
-//    if(pCodec == NULL){
-//       FFLOGI("codec not found");
-//       return -1;
+//    // Find the decoder for the video stream
+//    AVCodec *pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
+//    if (pCodec == NULL) {
+//        LOGD("Codec not found.");
+//        return -1; // Codec not found
 //    }
 //
-//    if(avcodec_open2(pCodecContext,pCodec,NULL) < 0){
-//       FFLOGI("can not open codec");
-//       return -1;
+//    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+//        LOGD("Could not open codec.");
+//        return -1; // Could not open codec
 //    }
 //
-//    ANativeWindow *nativeWindow = ANativeWindow_fromSurface(env,surface);
+//    // 获取native window
+//    ANativeWindow *nativeWindow = ANativeWindow_fromSurface(env, surface);
 //
-//    int videoWidth = pCodecContext -> width;
-//    int videoHeight = pCodecContext -> height;
+//    // 获取视频宽高
+//    int videoWidth = pCodecCtx->width;
+//    int videoHeight = pCodecCtx->height;
 //
-//    ANativeWindow_setBuffersGeometry(nativeWindow,videoWidth,videoHeight,WINDOW_FORMAT_RGBA_8888);
-//
+//    // 设置native window的buffer大小,可自动拉伸
+//    ANativeWindow_setBuffersGeometry(nativeWindow, videoWidth, videoHeight,
+//                                     WINDOW_FORMAT_RGBA_8888);
 //    ANativeWindow_Buffer windowBuffer;
 //
-//    if(avcodec_open2(pCodecContext,pCodec,NULL) < 0){
-//       FFLOGI("can not open codec ");
-//       return -1;
+//    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+//        LOGD("Could not open codec.");
+//        return -1; // Could not open codec
 //    }
 //
+//    // Allocate video frame
 //    AVFrame *pFrame = av_frame_alloc();
 //
+//    // 用于渲染
 //    AVFrame *pFrameRGBA = av_frame_alloc();
-//    if(pFrameRGBA == NULL || pFrame ==NULL){
-//       FFLOGI(" can not allocate video frame ");
-//       return -1;
+//    if (pFrameRGBA == NULL || pFrame == NULL) {
+//        LOGD("Could not allocate video frame.");
+//        return -1;
 //    }
 //
-//    int numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGBA,pCodecContext->width,pCodecContext->height,1);
-//    uint8_t *buffer = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
-//    av_image_fill_arrays(pFrameRGBA->data,pFrameRGBA->linesize,buffer,AV_PIX_FMT_RGBA,
-//                                          pCodecContext->width,pCodecContext->height,1);
+//    // Determine required buffer size and allocate buffer
+//    // buffer中数据就是用于渲染的,且格式为RGBA
+//    int numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGBA, pCodecCtx->width, pCodecCtx->height,
+//                                            1);
+//    uint8_t *buffer = (uint8_t *) av_malloc(numBytes * sizeof(uint8_t));
+//    av_image_fill_arrays(pFrameRGBA->data, pFrameRGBA->linesize, buffer, AV_PIX_FMT_RGBA,
+//                         pCodecCtx->width, pCodecCtx->height, 1);
 //
-//    struct SwsContext *sws_ctx = sws_getContext(pCodecContext->width,pCodecContext->height,
-//                                                pCodecContext->pix_fmt,
-//                                                pCodecContext->width,pCodecContext->height,
-//                                                AV_PIX_FMT_RGBA,SWS_BILINEAR,
-//                                                NULL,NULL,NULL);
+//    // 由于解码出来的帧格式不是RGBA的,在渲染之前需要进行格式转换
+//    struct SwsContext *sws_ctx = sws_getContext(pCodecCtx->width,
+//                                                pCodecCtx->height,
+//                                                pCodecCtx->pix_fmt,
+//                                                pCodecCtx->width,
+//                                                pCodecCtx->height,
+//                                                AV_PIX_FMT_RGBA,
+//                                                SWS_BILINEAR,
+//                                                NULL,
+//                                                NULL,
+//                                                NULL);
 //
 //    int frameFinished;
 //    AVPacket packet;
-//    while(av_read_frame(pFormatCtx,&packet) >= 0){
-//        if(packet.stream_index == videoStream){
-//           avcodec_decode_video2(pCodecContext,pFrame,&frameFinished,&packet);
-//           if(frameFinished){
-//              ANativeWindow_lock(nativeWindow,&windowBuffer,0);
-//              sws_scale(sws_ctx,(uint8_t const *const *)pFrame->data,
-//                        pFrame->linesize,0,pCodecContext->height,
-//                        pFrameRGBA->data,pFrameRGBA->linesize);
+//    while (av_read_frame(pFormatCtx, &packet) >= 0) {
+//        // Is this a packet from the video stream?
+//        if (packet.stream_index == videoStream) {
 //
-//              uint8_t *dst = (uint8_t *) windowBuffer.bits;
-//              int dstStride = windowBuffer.stride * 4;
-//              uint8_t *src = (pFrameRGBA->data[0]);
-//              int srcStride = pFrameRGBA->linesize[0];
+//            // Decode video frame
+//            avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
 //
-//              int h;
-//              for(h = 0 ; h < videoHeight ; h++){
-//                 memcpy(dst + h * dstStride , src + h * srcStride , srcStride);
-//              }
+//            // 并不是decode一次就可解码出一帧
+//            if (frameFinished) {
 //
-//              ANativeWindow_unlockAndPost(nativeWindow);
-//           }
+//                // lock native window buffer
+//                ANativeWindow_lock(nativeWindow, &windowBuffer, 0);
+//
+//                // 格式转换
+//                sws_scale(sws_ctx, (uint8_t const *const *) pFrame->data,
+//                          pFrame->linesize, 0, pCodecCtx->height,
+//                          pFrameRGBA->data, pFrameRGBA->linesize);
+//
+//                // 获取stride
+//                uint8_t *dst = (uint8_t *) windowBuffer.bits;
+//                int dstStride = windowBuffer.stride * 4;
+//                uint8_t *src = (pFrameRGBA->data[0]);
+//                int srcStride = pFrameRGBA->linesize[0];
+//
+//                // 由于window的stride和帧的stride不同,因此需要逐行复制
+//                int h;
+//                for (h = 0; h < videoHeight; h++) {
+//                    memcpy(dst + h * dstStride, src + h * srcStride, srcStride);
+//                }
+//
+//                ANativeWindow_unlockAndPost(nativeWindow);
+//            }
+//
 //        }
 //        av_packet_unref(&packet);
 //    }
+//
 //    av_free(buffer);
 //    av_free(pFrameRGBA);
+//
+//    // Free the YUV frame
 //    av_free(pFrame);
 //
-//    avcodec_close(pCodecContext);
+//    // Close the codecs
+//    avcodec_close(pCodecCtx);
+//
+//    // Close the video file
 //    avformat_close_input(&pFormatCtx);
-    return 0;
+//    return 0;
 }
--- app/src/main/java/com/example/wingbu/ffmpegbasic/MainActivity.java
+++ app/src/main/java/com/example/wingbu/ffmpegbasic/MainActivity.java
@@ -4,16 +4,20 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.TextView;
 
 import java.io.File;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback{
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+
+    SurfaceHolder mSurfaceHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +28,43 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJHello());
 
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+        mSurfaceHolder = surfaceView.getHolder();
+        mSurfaceHolder.addCallback(this);
+
         String input = new File("sdcard/ff-input/example.mp4").getAbsolutePath();
         String output = new File("sdcard/ff-input/carphone_qcif.yuv").getAbsolutePath();
         String s = Environment.getExternalStorageDirectory().toString();
         Log.i("Wings", Environment.getExternalStorageDirectory().toString());
 //        File file = new File("sdcard/sintel.mp4");
 //        boolean is = file.exists();
-        decode("sdcard/ff-input/example.mp4", "sdcard/ff-input/carphone_qcif.yuv");
+        //decode("sdcard/ff-input/example.mp4", "sdcard/ff-input/carphone_qcif.yuv");
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.i("ffmpeg"," surfaceCreated ");
+                play("sdcard/ff-input/example.avi",mSurfaceHolder.getSurface());
+            }
+        }).start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 
     /**
@@ -40,4 +74,5 @@ public class MainActivity extends AppCompatActivity {
     public native String stringFromJNI();
     public native String stringFromJHello();
     public native static void decode(String input,String output);
+    public native int play(String input_,Object surface);
 }
