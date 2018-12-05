b76fabdd59a0b8d4a50b6c347adec9c3d987b725
Wingbu <949540717@qq.com>
2018-12-05 22:45:14 +0800

    add index activity first

--- app/src/main/java/com/example/wingbu/ffmpegbasic/IndexActivity.java
+++ app/src/main/java/com/example/wingbu/ffmpegbasic/IndexActivity.java
@@ -2,12 +2,79 @@ package com.example.wingbu.ffmpegbasic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IndexActivity extends AppCompatActivity {
+
+    private Button mBtnOne;
+    private Button mBtnTwo;
+    private Button mBtnThree;
+    private Button mBtnFour;
+    private Button mBtnFive;
+    private Button mBtnSix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        initView();
     }
+
+    private void initView(){
+        mBtnOne = (Button) findViewById(R.id.btn_index_1);
+        mBtnTwo = (Button) findViewById(R.id.btn_index_2);
+        mBtnThree = (Button) findViewById(R.id.btn_index_3);
+        mBtnFour = (Button) findViewById(R.id.btn_index_4);
+        mBtnFive = (Button) findViewById(R.id.btn_index_5);
+        mBtnSix = (Button) findViewById(R.id.btn_index_6);
+
+        mBtnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mBtnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mBtnThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mBtnFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mBtnFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mBtnSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void jumpToRawPlayMusic(){}
+
+    private void jumpToPlayVideo(){}
+
+    private void jumpToMixPlayVideo(){}
 }
