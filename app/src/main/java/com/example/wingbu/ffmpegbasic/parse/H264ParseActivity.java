772eba5bf62d253f339c6d1523da1ff9fd5ac4ba
Wingbu <949540717@qq.com>
2018-12-24 22:24:42 +0800

    add h264 parse activity

---
+++ app/src/main/java/com/example/wingbu/ffmpegbasic/parse/H264ParseActivity.java
@@ -0,0 +1,15 @@
+package com.example.wingbu.ffmpegbasic.parse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wingbu.ffmpegbasic.R;
+
+public class H264ParseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_h264_parse);
+    }
+}
