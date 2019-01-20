9bbc5a1990dc5425f79eac525fde6dfde65023f5
Wingbu <949540717@qq.com>
2019-01-20 19:39:14 +0800

    trans pcm to wav

--- app/src/main/cpp/native-deal-pcm.cpp
+++ app/src/main/cpp/native-deal-pcm.cpp
@@ -221,6 +221,94 @@ Java_com_example_wingbu_ffmpegbasic_trans_TransPCMActivity_cutPcm(JNIEnv *env, j
 extern "C"
 JNIEXPORT jint JNICALL
 Java_com_example_wingbu_ffmpegbasic_trans_TransPCMActivity_transPcmToWave(JNIEnv *env, jclass type,
-                                                                         jstring inputFilePath, jstring outputFilePath) {
+                                                                         jstring inputFilePath, jint channels,jint sample_rate, jstring outputFilePath ) {
+    typedef struct WAVE_HEADER{
+        char         fccID[4];
+        unsigned   long    dwSize;
+        char         fccType[4];
+    }WAVE_HEADER;
+
+    typedef struct WAVE_FMT{
+        char         fccID[4];
+        unsigned   long       dwSize;
+        unsigned   short     wFormatTag;
+        unsigned   short     wChannels;
+        unsigned   long       dwSamplesPerSec;
+        unsigned   long       dwAvgBytesPerSec;
+        unsigned   short     wBlockAlign;
+        unsigned   short     uiBitsPerSample;
+    }WAVE_FMT;
+
+    typedef struct WAVE_DATA{
+        char       fccID[4];
+        unsigned long dwSize;
+    }WAVE_DATA;
+
+    if(channels==0||sample_rate==0){
+        channels = 2;
+        sample_rate = 44100;
+    }
+    int bits = 16;
+
+    WAVE_HEADER   pcmHEADER;
+    WAVE_FMT   pcmFMT;
+    WAVE_DATA   pcmDATA;
+
+    unsigned   short   m_pcmData;
+    FILE   *fp,*fpout;
+
+    //获取输入输出文件名
+    const char *input_path = env->GetStringUTFChars(inputFilePath, 0);
+    const char *output_path = env->GetStringUTFChars(outputFilePath, 0);
+
+    fp=fopen(input_path, "rb");
+    if(fp == NULL) {
+        printf("open pcm file error\n");
+        return -1;
+    }
+    fpout=fopen(output_path, "wb+");
+    if(fpout == NULL) {
+        printf("create wav file error\n");
+        return -1;
+    }
+
+    //WAVE_HEADER
+    memcpy(pcmHEADER.fccID,"RIFF",strlen("RIFF"));
+    memcpy(pcmHEADER.fccType,"WAVE",strlen("WAVE"));
+    fseek(fpout,sizeof(WAVE_HEADER),1);
+    //WAVE_FMT
+    pcmFMT.dwSamplesPerSec=sample_rate;
+    pcmFMT.dwAvgBytesPerSec=pcmFMT.dwSamplesPerSec*sizeof(m_pcmData);
+    pcmFMT.uiBitsPerSample=bits;
+    memcpy(pcmFMT.fccID,"fmt ",strlen("fmt "));
+    pcmFMT.dwSize=16;
+    pcmFMT.wBlockAlign=2;
+    pcmFMT.wChannels=channels;
+    pcmFMT.wFormatTag=1;
+
+    fwrite(&pcmFMT,sizeof(WAVE_FMT),1,fpout);
+
+    //WAVE_DATA;
+    memcpy(pcmDATA.fccID,"data",strlen("data"));
+    pcmDATA.dwSize=0;
+    fseek(fpout,sizeof(WAVE_DATA),SEEK_CUR);
+
+    fread(&m_pcmData,sizeof(unsigned short),1,fp);
+    while(!feof(fp)){
+        pcmDATA.dwSize+=2;
+        fwrite(&m_pcmData,sizeof(unsigned short),1,fpout);
+        fread(&m_pcmData,sizeof(unsigned short),1,fp);
+    }
+
+    pcmHEADER.dwSize=44+pcmDATA.dwSize;
+
+    rewind(fpout);
+    fwrite(&pcmHEADER,sizeof(WAVE_HEADER),1,fpout);
+    fseek(fpout,sizeof(WAVE_FMT),SEEK_CUR);
+    fwrite(&pcmDATA,sizeof(WAVE_DATA),1,fpout);
+
+    fclose(fp);
+    fclose(fpout);
+
     return 0;
 }
